bedccef7ed2dfd9d7f950e8e3efb5b4ab48dea77
anshu <anshu.ac.dv@gmail.com>
2026-02-12 21:21:27 +0530

    Initial Commit

--- lib/UI/Auth/sign_up.dart
+++ lib/UI/Auth/sign_up.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/UI/Auth/login_screen.dart';
+import 'package:flutter_firebase/Widgets/button.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class SignUp extends StatelessWidget {
+class SignUp extends StatefulWidget {
   const SignUp({super.key});
 
   @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+    final nameController = TextEditingController();
+    final emailController = TextEditingController();
+    final passwordController = TextEditingController();
+
+    void dispose() {
+      nameController.dispose();
+      emailController.dispose();
+      passwordController.dispose();
+      super.dispose();
+    }
+
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
@@ -32,6 +51,96 @@ class SignUp extends StatelessWidget {
               "Register Now",
               style: GoogleFonts.oswald(fontSize: 25, color: Colors.black),
             ),
+          ),
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+                  child: TextFormField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      hintText: "Name",
+                      prefixIcon: const Icon(Icons.drive_file_rename_outline),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your name";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+                  child: TextFormField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      hintText: "Email",
+                      prefixIcon: const Icon(Icons.email),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your Email";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+                  child: TextFormField(
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      suffixIcon: const Icon(Icons.remove_red_eye),
+                      hintText: "Password",
+                      prefixIcon: const Icon(Icons.password),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please create your password";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 40, left: 20, right: 20),
+            child: Button(
+              title: "Register Now",
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {}
+              },
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text("Already have an account?"),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    (context),
+                    MaterialPageRoute(builder: (context) => LoginScreen()),
+                  );
+                },
+                child: const Text("Login Now"),
+              ),
+            ],
           ),
         ],
       ),
