9d06a1dde668317e152b448eef17f7db3275c7c1
anshu <anshu.ac.dv@gmail.com>
2026-02-15 17:08:39 +0530

    Initial Commit

--- lib/UI/Auth/welcome.dart
+++ lib/UI/Auth/welcome.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase/UI/Auth/login_screen.dart';
+import 'package:flutter_firebase/UI/add_post.dart';
 import 'package:flutter_firebase/Utilits/errorToast.dart';
 
 class Welcome extends StatefulWidget {
@@ -38,6 +39,15 @@ class _WelcomeState extends State<Welcome> {
             icon: Icon(Icons.logout),
           ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            (context),
+            MaterialPageRoute(builder: (context) => AddPost()),
+          );
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
